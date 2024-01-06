f95ddf7843faab8aaf5bc712a2de250df34811b3
Arjun Rana <gemthearjun@gmail.com>
2024-01-06 20:14:06 +0530

    setting Global Constants for colors

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,12 +1,14 @@
 // imports
 import { View, Text, Pressable, StyleSheet } from "react-native";
+import Colors from "../constants/colors";
+import { clickProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
 function PrimaryButton({ children, onPress }) {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
         onPress={onPress}
-        android_ripple={{ color: "#640233" }}
+        android_ripple={{ color: Colors.primary600 }}
         style={({ pressed }) =>
           pressed
             ? [styles.buttonInnerContainer, styles.pressed]
@@ -23,7 +25,7 @@ export default PrimaryButton;
 
 const styles = StyleSheet.create({
   buttonInnerContainer: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
 
     paddingVertical: 8,
     paddingHorizontal: 16,
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,8 +2,9 @@
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import { useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
+import Colors from "../constants/colors";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -31,7 +32,7 @@ function StartGameScreen() {
       );
       return;
     }
-    console.log("Valid Number.");
+    onPickNumber(chosenNumber);
   }
   return (
     <View style={styles.inputContainer}>
@@ -66,7 +67,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: "#3b021f",
+    backgroundColor: Colors.primary800,
     elevation: 4,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
@@ -78,9 +79,9 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor: "#ddb52f",
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
